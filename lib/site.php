125871cbb647f011712e5bc5f0174afa4a6976e5
Steve Pfisterer <steve@stoutlogic.com>
2015-09-03 22:29:23 -0500

    Add support function for customizing admin menu

--- lib/site.php
+++ lib/site.php
@@ -29,6 +29,8 @@ class Site extends \TimberSite
         add_action('init', array( $this, 'registerNavigations' ));
         add_action('init', array( $this, 'registerOptionPages' ));
 
+        add_action('admin_init', array( $this, 'customizeAdminMenu' ), 10);
+
         add_action('wp_enqueue_scripts', array( $this, 'enqueScripts'), 100);
         add_action('wp_enqueue_scripts', array( $this, 'enqueDefaultStylesheets'), 100000);
         add_action('wp_enqueue_scripts', array( $this, 'enqueStylesheets'), 102);
@@ -89,6 +91,11 @@ class Site extends \TimberSite
     }
 
     public function registerOptionPages()
+    {
+        
+    }
+
+    public function customizeAdminMenu()
     {
         
     }
