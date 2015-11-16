8ccd81253a0ad63a50c6c77ee35bd8d0fa1d536c
Steve Pfisterer <steve@stoutlogic.com>
2015-11-16 14:35:17 -0600

    Have User implement HasMetaData

--- lib/user.php
+++ lib/user.php
@@ -2,7 +2,7 @@
 
 namespace Understory;
 
-class User extends \TimberUser
+class User extends \TimberUser implements HasMetaData
 {
     public function isLoggedIn()
     {
@@ -11,5 +11,27 @@ class User extends \TimberUser
         }
 
         return false;
+    }
+
+    /**
+     * Implentation of HasMetaData->getMetaValue
+     *
+     * @param  string $metaFieldKey Key for the meta field
+     * @return string                Value of the meta field
+     */
+    public function getMetaValue($key)
+    {
+        return \get_user_meta($this->ID, $key, true);
+    }
+    
+    /**
+     * Implentation of HasMetaData->setMetaValue
+     *
+     * @param  string $key Key for the meta field
+     * @param  string $value Value for the meta field
+     */
+    public function setMetaValue($key, $value)
+    {
+        \update_user_meta($this->ID, $key, $value);
     }
 }
