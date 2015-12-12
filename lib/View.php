b52fbb07e004df26da7afe9e87481ddb38cc0d93
Steve Pfisterer <stevepfisterer@gmail.com>
2015-12-12 14:38:05 -0600

    Have the View class implement HasMetaData, which delegates to the post retrieved via getPost

--- lib/View.php
+++ lib/View.php
@@ -12,7 +12,7 @@ namespace Understory;
 /**
  * Understory View
  */
-class View
+class View implements HasMetaData
 {
     use Core;
 
@@ -122,5 +122,27 @@ class View
     public function render()
     {
         \Timber::render($this->getTemplate(), $this->getContext());
+    }
+
+    /**
+     * Implentation of HasMetaData->getMetaValue
+     *
+     * @param  string $metaFieldKey Key for the meta field
+     * @return string                Value of the meta field
+     */
+    public function getMetaValue($key)
+    {
+        return \get_post_meta($this->getPost()->ID, $key, true);
+    }
+    
+    /**
+     * Implentation of HasMetaData->setMetaValue
+     *
+     * @param  string $key Key for the meta field
+     * @param  string $value Value for the meta field
+     */
+    public function setMetaValue($key, $value)
+    {
+        \update_post_meta($this->getPost()->ID, $key, $value);
     }
 }
