f144361a67518c29477eef625d546dbdbd704b39
Steve Pfisterer <steve@stoutlogic.com>
2019-07-09 19:03:48 -0500

    fix regression where understory parent themes views were not being found

--- lib/init.php
+++ lib/init.php
@@ -8,6 +8,11 @@
  */
 \add_filter('template_directory', function ($template_dir, $template, $theme_root) {
     if (!defined('TEMPLATEPATH')) {
+        // Bail if parent theme isn't an Understory Theme
+        if (!file_exists($template_dir . '/app/Site.php')) {
+            return $template_dir;
+        }
+
         if (file_exists($template_dir . '/app/Views')) {
             $template_dir .= '/app/Views';
         }
@@ -26,24 +31,30 @@
     return $template_dir;
 }, 10000, 3);
 
+// If our custom template name contains the /app/Views path in it, remove it.
+\add_filter('page_template_hierarchy', function($templates) {
+    return array_map(function ($template) {
+        return str_replace('app/Views/', '', $template);
+    }, $templates);
+}, 10000);
 
 
 \add_filter('theme_page_templates', function ($page_templates, $theme, $post, $posttype) {
 
-     $files = (array) $theme->get_files('php', 2, true);
+    $files = (array) $theme->get_files('php', 2, true);
 
-        foreach ($files as $file => $full_path) {
-            if (! preg_match('|Template Name:(.*)$|mi', file_get_contents($full_path), $header)) {
-                continue;
-            }
-            $page_templates[ $file ] = _cleanup_header_comment($header[1]);
+    foreach ($files as $file => $full_path) {
+        if (! preg_match('|Template Name:(.*)$|mi', file_get_contents($full_path), $header)) {
+            continue;
         }
+        $page_templates[ $file ] = _cleanup_header_comment($header[1]);
+    }
 
-        // Hack by adding to the cache until WordPress allows us to add templates
-        $cache_hash = md5( $theme->get_stylesheet_directory() );
-        $key = "page_templates-{$cache_hash}";
-        $group = 'themes';
-        wp_cache_set( $key, $page_templates, $group, 1800 );
+    // Hack by adding to the cache until WordPress allows us to add templates
+    $cache_hash = md5( $theme->get_stylesheet_directory() );
+    $key = "page_templates-{$cache_hash}";
+    $group = 'themes';
+    wp_cache_set( $key, $page_templates, $group, 1800 );
 
     return $page_templates;
 
